8b3557cf0cb6a4332503f4a2a756212c90ed18df
arnav-verma22 <arn.verma22@gmail.com>
2022-01-04 14:19:03 +0530

    app.jsx

---
+++ trying-to-react/src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from "react";
+import NavBar from "./Navbar";
+
+function App() {
+  return (
+    <NavBar />
+  );
+}
+
+export default App;
